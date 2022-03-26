720cf61b3a121ef4f6d3797d2deae695a7e7c69e
Silvano <silvano@superroot.eu>
2022-03-26 09:36:27 +0000

    Update codepipeline_service_policy policy, remove loads unnecessary actions

--- terraform_tooling/code_pipeline.tf
+++ terraform_tooling/code_pipeline.tf
@@ -133,20 +133,10 @@ resource "aws_iam_policy" "codepipeline_service_policy" {
     "Statement": [
         {
             "Action": [
-                "iam:PassRole"
+                "s3:*"
             ],
             "Resource": "*",
-            "Effect": "Allow",
-            "Condition": {
-                "StringEqualsIfExists": {
-                    "iam:PassedToService": [
-                        "cloudformation.amazonaws.com",
-                        "elasticbeanstalk.amazonaws.com",
-                        "ec2.amazonaws.com",
-                        "ecs-tasks.amazonaws.com"
-                    ]
-                }
-            }
+            "Effect": "Allow"
         },
         {
             "Action": [
@@ -157,82 +147,8 @@ resource "aws_iam_policy" "codepipeline_service_policy" {
                 "codecommit:GetUploadArchiveStatus",
                 "codecommit:UploadArchive"
             ],
-            "Resource": "*",
-            "Effect": "Allow"
-        },
-        {
-            "Action": [
-                "codedeploy:CreateDeployment",
-                "codedeploy:GetApplication",
-                "codedeploy:GetApplicationRevision",
-                "codedeploy:GetDeployment",
-                "codedeploy:GetDeploymentConfig",
-                "codedeploy:RegisterApplicationRevision"
-            ],
-            "Resource": "*",
-            "Effect": "Allow"
-        },
-        {
-            "Action": [
-                "codestar-connections:UseConnection"
-            ],
-            "Resource": "*",
-            "Effect": "Allow"
-        },
-        {
-            "Action": [
-                "elasticbeanstalk:*",
-                "ec2:*",
-                "elasticloadbalancing:*",
-                "autoscaling:*",
-                "cloudwatch:*",
-                "s3:*",
-                "sns:*",
-                "cloudformation:*",
-                "rds:*",
-                "sqs:*",
-                "ecs:*"
-            ],
-            "Resource": "*",
-            "Effect": "Allow"
-        },
-        {
-            "Action": [
-                "lambda:InvokeFunction",
-                "lambda:ListFunctions"
-            ],
-            "Resource": "*",
-            "Effect": "Allow"
-        },
-        {
-            "Action": [
-                "opsworks:CreateDeployment",
-                "opsworks:DescribeApps",
-                "opsworks:DescribeCommands",
-                "opsworks:DescribeDeployments",
-                "opsworks:DescribeInstances",
-                "opsworks:DescribeStacks",
-                "opsworks:UpdateApp",
-                "opsworks:UpdateStack"
-            ],
-            "Resource": "*",
-            "Effect": "Allow"
-        },
-        {
-            "Action": [
-                "cloudformation:CreateStack",
-                "cloudformation:DeleteStack",
-                "cloudformation:DescribeStacks",
-                "cloudformation:UpdateStack",
-                "cloudformation:CreateChangeSet",
-                "cloudformation:DeleteChangeSet",
-                "cloudformation:DescribeChangeSet",
-                "cloudformation:ExecuteChangeSet",
-                "cloudformation:SetStackPolicy",
-                "cloudformation:ValidateTemplate"
-            ],
-            "Resource": "*",
-            "Effect": "Allow"
+            "Effect": "Allow",
+            "Resource": "*"
         },
         {
             "Action": [
@@ -241,86 +157,8 @@ resource "aws_iam_policy" "codepipeline_service_policy" {
                 "codebuild:BatchGetBuildBatches",
                 "codebuild:StartBuildBatch"
             ],
-            "Resource": "*",
-            "Effect": "Allow"
-        },
-        {
             "Effect": "Allow",
-            "Action": [
-                "devicefarm:ListProjects",
-                "devicefarm:ListDevicePools",
-                "devicefarm:GetRun",
-                "devicefarm:GetUpload",
-                "devicefarm:CreateUpload",
-                "devicefarm:ScheduleRun"
-            ],
             "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "servicecatalog:ListProvisioningArtifacts",
-                "servicecatalog:CreateProvisioningArtifact",
-                "servicecatalog:DescribeProvisioningArtifact",
-                "servicecatalog:DeleteProvisioningArtifact",
-                "servicecatalog:UpdateProduct"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "cloudformation:ValidateTemplate"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ecr:DescribeImages"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "states:DescribeExecution",
-                "states:DescribeStateMachine",
-                "states:StartExecution"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "appconfig:StartDeployment",
-                "appconfig:StopDeployment",
-                "appconfig:GetDeployment"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Resource": [
-                "${aws_s3_bucket.my_resource.arn}",
-                "${aws_s3_bucket.my_resource.arn}/*"
-            ],
-            "Action": [
-                "s3:PutObject",
-                "s3:GetBucketAcl",
-                "s3:GetBucketLocation"
-            ]
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "kms:Encrypt",
-                "kms:Decrypt",
-                "kms:ReEncrypt*",
-                "kms:GenerateDataKey*",
-                "kms:DescribeKey"
-            ],
-            "Resource": "${aws_kms_key.my_resource.arn}"
         },
         {
             "Action": "sts:AssumeRole",
